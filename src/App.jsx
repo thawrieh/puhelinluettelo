bded31b65a9f3364d6295ec620c6076aa88bb3e4
s2300283 <s2300283@edubc.fi>
2023-11-21 23:36:14 +0200

    task2.8

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const handleFormSubmit = (event) => {
@@ -15,9 +20,10 @@ const App = () => {
     if (isNameAlreadyAdded) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      const newPerson = { name: newName }
+      const newPerson = { name: newName, number: newNumber }
       setPersons([...persons, newPerson])
-      setNewName('')
+      setNewName('') 
+      setNewNumber('')
     }
   }
 
@@ -29,12 +35,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
         {persons.map((person, index) => (
-          <p key={index}>{person.name}</p>
+          <p key={index}>{person.name}  {person.number}</p>
         ))}
     </div>
   )
